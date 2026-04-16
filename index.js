86e921be4d9b25c85790f8b8571b365171c06f84
Vinay Phanse <vinayphanse14@gmail.com>
2026-04-16 18:47:35 +0530

    github data added

--- index.js
+++ index.js
@@ -4,6 +4,42 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const githubData = {
+    "login": "vinayphanse888",
+    "id": 191205702,
+    "node_id": "U_kgDOC2WRRg",
+    "avatar_url": "https://avatars.githubusercontent.com/u/191205702?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/vinayphanse888",
+    "html_url": "https://github.com/vinayphanse888",
+    "followers_url": "https://api.github.com/users/vinayphanse888/followers",
+    "following_url": "https://api.github.com/users/vinayphanse888/following{/other_user}",
+    "gists_url": "https://api.github.com/users/vinayphanse888/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/vinayphanse888/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/vinayphanse888/subscriptions",
+    "organizations_url": "https://api.github.com/users/vinayphanse888/orgs",
+    "repos_url": "https://api.github.com/users/vinayphanse888/repos",
+    "events_url": "https://api.github.com/users/vinayphanse888/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/vinayphanse888/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false,
+    "name": "Vinay Phanse",
+    "company": null,
+    "blog": "",
+    "location": "Mumbai",
+    "email": null,
+    "hireable": null,
+    "bio": "B.Tech Information Technology student passionate about Frontend Development. Find me on LinkedIn: www.linkedin.com/in/vinayphanse",
+    "twitter_username": null,
+    "public_repos": 8,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 0,
+    "created_at": "2024-12-10T05:47:24Z",
+    "updated_at": "2026-04-04T14:21:57Z"
+}
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -14,6 +50,10 @@ app.get('/twitter', (req, res) => {
 
 app.get('/login', (req, res) => {
     res.send('<h1>Please login to this page</h1>')
+})
+
+app.get('/github', (req, res) => {
+    res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
